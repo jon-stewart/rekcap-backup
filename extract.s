f4babf2328337f6515c5099b65ef79f458a77738
jon-stewart <jon.linux@ssl-mail.com>
2016-10-07 21:21:59 +0100

    hello world position independant assembly

---
+++ extract.s
@@ -0,0 +1,47 @@
+;------------------------------------------------------------------------------
+; Name:
+;   extract
+;
+; Description:
+;   Stub of code placed along side packed elf executable.
+;
+;   Elf hdr e_entry should point to this code, it will then unpack original
+;   executable into memory.
+;
+[section .text]
+
+[BITS 64]
+
+; stack
+; [rbp-0x8] : base address
+
+global _start
+
+_start:
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 4
+    call    delta
+
+delta:
+    pop     rax
+    sub     rax, delta
+    mov     [rbp-8], rax
+
+    mov     rax, 1              ; sys_write
+    mov     rdi, 1              ; stdout
+    mov     rdx, [rbp-8]
+    lea     rsi, [rdx + str]    ; buf
+    mov     rdx, str_sz         ; count
+    syscall
+
+exit:
+    mov     rsp, rbp
+    pop     rbp
+
+    mov     eax, 60
+    xor     rdi, rdi
+    syscall
+
+str:     db "Hello world",10,0
+str_sz:  equ $-str
