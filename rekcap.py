3b04d8f6ca1a4aef36cf9517bf028fb2a3c52351
jon-stewart <jon.linux@ssl-mail.com>
2016-10-08 16:18:35 +0100

    Reading binary_to_be_packed and xor(0x90)

--- rekcap.py
+++ rekcap.py
@@ -1,15 +1,50 @@
 #!/usr/bin/python3
 
 from elf import Elf
+import struct
 
-with open("extract", "rb") as fp:
-    stub_data = fp.read()
+
+def read_file(fname):
+    data = None
+
+    with open(fname, "rb") as fp:
+        data = fp.read()
+
+    return (data)
+
+
+def write_file(fname, data):
+    with open(fname, "wb") as fp:
+        fp.write(data)
+
+
+def xor_data(fdata):
+    if (type(fdata) is not bytes):
+        print("[!] Expecting bytes object")
+        exit(-1)
+
+    data = struct.unpack("%dB" % len(fdata), fdata)
+
+    out = []
+    for c in data:
+        out.append(c ^ 0x90)
+
+    return (struct.pack("%dB" % len(out), *out))
+
+
+extract_stub = read_file("extract")
+
+binary = xor_data(read_file("example"))
 
 elf = Elf()
 
 elf.create_ehdr()
 
-elf.create_phdr(0, 0x800000, 0xf0, stub_data)
+# TODO auto generate offset/size
+elf.create_phdr(0, 0x800000, 0xf0, extract_stub)
+
+# TODO what offset?
+#elf.create_phdr(0, 0x400000, len(binary), binary)
 
 elf.padding(200)
 
